01001c7e9e2e263b6d39000f7946882636f36477
sayedulsayem <sayedulsayem@gmail.com>
2024-01-29 23:21:56 +0600

    make it simple

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -6,7 +6,6 @@ import fn from "../functions.js";
 const newTaskInput = ref("");
 let nextId = todos.length + 1;
 const currentEditing = ref(null);
-const currentTab = ref("all");
 
 const getStorageData = () => {
   todos.length = 0;
@@ -18,24 +17,6 @@ const getStorageData = () => {
 };
 
 getStorageData();
-
-const getTodos = () => {
-  if (currentTab.value == "all") {
-    return todos;
-  } else if (currentTab.value == "active") {
-    return todos.filter((todo) => {
-      if (!todo.done) {
-        return todo;
-      }
-    });
-  } else if (currentTab.value == "completed") {
-    return todos.filter((todo) => {
-      if (todo.done) {
-        return todo;
-      }
-    });
-  }
-};
 
 const addNewTodo = () => {
   todos.unshift({ id: nextId, task: newTaskInput.value, done: false });
@@ -51,7 +32,7 @@ const toggleTaskStatus = () => {
 const deleteTask = (id) => {
   let tempTodos = JSON.parse(JSON.stringify(todos));
   todos.length = 0;
-  tempTodos = tempTodos.map((todo) => {
+  tempTodos.forEach((todo) => {
     if (todo.id != id) {
       todos.push(todo);
     }
@@ -59,43 +40,9 @@ const deleteTask = (id) => {
   fn.updateTodosStorage(todos);
 };
 
-const clearTodos = () => {
-  if (currentTab.value == "all") {
-    todos.length = 0;
-  } else if (currentTab.value == "active") {
-    let tempTodos = JSON.parse(JSON.stringify(todos));
-    todos.length = 0;
-    tempTodos = tempTodos.map((todo) => {
-      if (todo.done) {
-        todos.push(todo);
-      }
-    });
-  } else if (currentTab.value == "completed") {
-    let tempTodos = JSON.parse(JSON.stringify(todos));
-    todos.length = 0;
-    tempTodos = tempTodos.map((todo) => {
-      if (!todo.done) {
-        todos.push(todo);
-      }
-    });
-  }
-  fn.updateTodosStorage(todos);
-};
-
 const updateTask = () => {
-    currentEditing.value = null;
-    fn.updateTodosStorage(todos);
-}
-
-const getRemainingText = () => {
-  let remaining = 0;
-  let total = todos.length;
-  todos.forEach((todo) => {
-    if (!todo.done) {
-      remaining++;
-    }
-  });
-  return `${remaining} of ${total} left`;
+  currentEditing.value = null;
+  fn.updateTodosStorage(todos);
 };
 </script>
 <template>
@@ -124,7 +71,7 @@ const getRemainingText = () => {
           </button>
         </form>
         <div class="h-80 overflow-x-hidden overflow-y-auto todo-list">
-          <template v-for="todo in getTodos()" :key="todo.id">
+          <template v-for="todo in todos" :key="todo.id">
             <div
               v-if="currentEditing != todo.id"
               class="shadow rounded-lg p-3 mt-4 bg-gray-50 dark:bg-slate-700 flex justify-between"
@@ -133,7 +80,11 @@ const getRemainingText = () => {
                 @dblclick="currentEditing = todo.id"
                 class="flex items-center"
               >
-                <input @change="toggleTaskStatus()" v-model="todo.done" type="checkbox" /><label
+                <input
+                  @change="toggleTaskStatus()"
+                  v-model="todo.done"
+                  type="checkbox"
+                /><label
                   class="ml-2 font-medium text-slate-800 dark:text-gray-50 text-sm"
                   :class="{ 'line-through': todo.done }"
                   >{{ todo.task }}</label
@@ -170,35 +121,9 @@ const getRemainingText = () => {
               </form>
             </div>
           </template>
-          <p v-if="getTodos().length <= 0" class="text-gray-100">
+          <p v-if="todos.length <= 0" class="text-gray-100">
             No todo available
           </p>
-        </div>
-        <div class="flex justify-between">
-          <div class="flex justify-center gap-5">
-            <p class="text-gray-100">{{ getRemainingText() }}</p>
-            <button
-              @click="currentTab = 'all'"
-              :class="currentTab == 'all' ? 'text-green-500' : 'text-gray-500'"
-            >
-              All</button
-            ><button
-              @click="currentTab = 'active'"
-              :class="
-                currentTab == 'active' ? 'text-green-500' : 'text-gray-500'
-              "
-            >
-              Active</button
-            ><button
-              @click="currentTab = 'completed'"
-              :class="
-                currentTab == 'completed' ? 'text-green-500' : 'text-gray-500'
-              "
-            >
-              Completed
-            </button>
-          </div>
-          <button @click="clearTodos()" class="text-red-600">Clear</button>
         </div>
       </div>
       <a href="https://sayedulsayem.com" target="_blank"
